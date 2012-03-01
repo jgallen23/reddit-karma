7b94c578900bf57108f24f2a15274c685e567cec
Greg Allen <me@jga.me>
2012-03-01 15:10:21 -0800

    deltas on every refresh

--- ui/scripts/app.js
+++ ui/scripts/app.js
@@ -12,13 +12,36 @@ var request = function(url, callback) {
 var renderUser = function(user) {
   request('http://www.reddit.com/user/'+user+'/about.json', function(data) {
     var tmp = $('[data-template=user]').html();
-    $("#user").template(tmp, { user: data.data });
+    var user = data.data;
+    getDelta(user, 'link_karma');
+    getDelta(user, 'comment_karma');
+    $("#user").template(tmp, { user: user });
   });
+};
+
+var getDelta = function(item, key) {
+  var fullKey = item.id+'_'+key;
+  var current = item[key];
+  var value = localStorage.getItem(fullKey);
+  var delta = 0;
+  if (value && value != current) {
+    delta = current - value;
+  }
+  localStorage.setItem(fullKey, current);
+  item[key+'_delta'] = delta;
 };
 
 var renderLinks = function(links) {
   var tmp = $('[data-template=links]').html();
+  for (var i = 0, c = links.length; i < c; i++) {
+    var item = links[i];
+    getDelta(item, 'score');
+    getDelta(item, 'ups');
+    getDelta(item, 'downs');
+    getDelta(item, 'num_comments');
+  }
   $("#links").template(tmp, { links: links });
+  showDeltas();
 };
 
 var renderComments = function(comments) {
@@ -38,6 +61,22 @@ var renderDetails = function(user) {
       }
     }
     renderLinks(links);
+  });
+};
+
+var showDeltas = function() {
+  $('.delta').each(function(i, item) {
+    var el = $(this);
+    var val = parseInt(el.text(), 10);
+    if (val === 0)
+      return;
+
+    if (val > 0) {
+      el.addClass('label-success');
+    } else {
+      el.addClass('label-important');
+    }
+    el.show();
   });
 };
 
